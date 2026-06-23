03f9930e9c5a16c750fb4b53316e3c5d4c2706aa
adakonda <adakondacoin@gmail.com>
2026-06-23 14:05:21 +0200

    Init assessment code

---
+++ server/config/config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    secretKey: 'A9f3b7c2D8F4eA1c6B0MZKxQpYH2WmN7R5JdL8sV4uT',
+    publicKey: 'aHR0cHM6Ly9hcGkuanNvbnN0b3JhZ2UubmV0L3YxL2pzb24vMmVmOGM3NTgtYTk2Zi00NTllLWIwMzYtYjNiOTAzNzlhMTY1L2ExNzllYTM1LWI5NjItNDcyMi1iM2YxLWUyODMxNmQxYTQ0YQ==',
+    localDB: 'mongodb://localhost/realestatedb'
+}
